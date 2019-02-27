8921ab0d54d3ce5603cbfc4ac9559194fc2d6210
shota sakai <lawliteqed@yahoo.co.jp>
2019-02-27 18:25:14 +0900

    vimrc for amazon linux2 , pyenv

---
+++ install.sh
@@ -0,0 +1,14 @@
+./configure --with-features=huge \
+--enable-multibyte \
+--enable-gpm \
+--enable-cscope \
+--enable-fontset \
+--enable-fail-if-missing \
+--enable-gui=no \
+--enable-python3interp=dynamic \
+--enable-luainterp \
+--enable-perlinterp \
+--prefix=/usr/local 
+
+#--with-python3-config-dir=/home/ec2-user/.pyenv/versions/3.7.2/lib/python3.7/config-3.7m-x86_64-linux-gnu \
+#--enable-python3interp \
